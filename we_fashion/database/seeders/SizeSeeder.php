2fc1b0deee58b5b8b0a83019d8ac5ce912d92343
corentin-koka <corentin@groupesanctuaire.com>
2023-04-24 12:21:04 +0200

    feat(Controller/seeder): add common function

---
+++ we_fashion/database/seeders/SizeSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SizeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sizes = ['XS', 'S', 'M', 'L', 'XL'];
+
+        foreach ($sizes as $size) {
+            DB::table('sizes')->insert([
+                'label' => $size,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
